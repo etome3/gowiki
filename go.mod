f7733a3a625da9198bb5b19ed6f7019dc4d965c5
etome3 <agfsabogus@gmail.com>
2026-01-16 17:38:53 +1000

    Initial project setup with basic HTTP server and HTMX template

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/etome3/gowiki
 
 go 1.25.5
+
+require (
+	github.com/gin-gonic/gin v1.10.0
+	github.com/google/uuid v1.6.0
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.Static("/static", "./static")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "GoWiki",
+		})
+	})
+
+	log.Println("Server starting on :8080")
+	log.Fatal(r.Run(":8080"))
+}
